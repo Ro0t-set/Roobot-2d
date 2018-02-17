daa1e057b6be906d21423a1759487b205faad616
Ro0t-set <ro0trexustom@gmail.com>
2018-02-17 17:49:33 +0100

    inizzio inteleigenza

--- robotica/punti.py
+++ robotica/punti.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-if __name__ == "__main__":
+if name == "main":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roobot.settings")
 
     from django.core.management import execute_from_command_line
@@ -18,9 +18,35 @@ from django.shortcuts import get_object_or_404
 
 Puntini.objects.create(nord=nord, sud=sud, est=est, overt=overt)
 
-
-
-
-
-
-
+# While (cas(x,y) = 0):
+#  cas(x,y) = 1
+#
+#  while angolo < 360:
+#    attiva sensore
+#    se c'è un ostacolo:
+#      calcola coordinate ostacolo
+#      approssimale a interi ottenendo x1 e y1
+#      cas(x1,y1) = 2
+#    angolo = angolo+15
+#  angolo=0
+#
+#  While (cas(x,y)) = 1):
+#    While (una delle caselle in un certo rettangolo davanti è nello stato 2):
+#
+#      girati a destra o sinistra casualmente
+#    se destra:  
+#      se nord -> est
+#      se est -> sud
+#      se sud -> ovest
+#      se ovest -> nord
+#    se sinistra:
+#      se nord -> ovest
+#      se ovest -> sud
+#      se sud -> est
+#      se est -> nord
+#
+#    Vai avanti di 3m
+#      se nord: y=y+3
+#      se sud: y=y-3
+#      se est: x=x+3
+#      se ovest: x=x-3
