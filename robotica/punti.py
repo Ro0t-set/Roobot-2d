1897e51acfbb93f3ed310fb6a053b81a5f0dbcee
arcobiSwifty <arcobi98@gmail.com>
2018-02-20 16:38:29 +0100

    Added Mapping Sistem
    MAPPING SISTEM NOT LINKED YET TO THE MAIN PART OF THE CODE.

--- robotica/punti.py
+++ robotica/punti.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
-
-if name == "main":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roobot.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
-
 import math
+
 import string
 from app.models import Puntini
 from django.shortcuts import get_object_or_404
 
-
+if name == "main":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roobot.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
 
 Puntini.objects.create(nord=nord, sud=sud, est=est, overt=overt)
 
@@ -25,6 +21,7 @@ Puntini.objects.create(nord=nord, sud=sud, est=est, overt=overt)
 #    attiva sensore
 #    se c'è un ostacolo:
 #      calcola coordinate ostacolo
+
 #      approssimale a interi ottenendo x1 e y1
 #      cas(x1,y1) = 2
 #    angolo = angolo+15
@@ -34,7 +31,7 @@ Puntini.objects.create(nord=nord, sud=sud, est=est, overt=overt)
 #    While (una delle caselle in un certo rettangolo davanti è nello stato 2):
 #
 #      girati a destra o sinistra casualmente
-#    se destra:  
+#    se destra:
 #      se nord -> est
 #      se est -> sud
 #      se sud -> ovest
@@ -50,3 +47,40 @@ Puntini.objects.create(nord=nord, sud=sud, est=est, overt=overt)
 #      se sud: y=y-3
 #      se est: x=x+3
 #      se ovest: x=x-3
+
+#1 = no obstacles
+#2 = obstacles
+#3 = robot
+
+def getXCorFromDistance(distance, angle):
+    x = Int(cos(angle)*distance)
+    return x
+def getYCorFromDistance(distance, angle):
+    y = Int(sin(angle)*distance)
+    return y
+
+def initCorSystemFromDistances(distances):
+    plot = dict() #init with no obstacles
+    for x in range(-20, 20):
+        newRow = dict()
+        for y in range(-20, 20):
+            if x == 0 & y == 0:
+                newRow[y] = 3
+            else:
+                newRow[y] = 1
+        plot[x] = newRow
+    angle = 0.0
+    count = 0
+    pi = math.pi
+    while angle < (2.0*pi):
+        angle = (angle+(2.0*pi)/float(distances.count))
+        x = getXCorFromDistance(distances[count], angle)
+        y = getYCorFromDistance(distances[count], angle)
+        plot[x][y] = 2
+        count = count+1
+    return plot
+
+class Tile():
+    column = Int()
+    row = Int()
+    tileType = Int() #enum if possible
