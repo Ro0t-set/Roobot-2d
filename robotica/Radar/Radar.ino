e760476d855a9d6c4758214658ec40a50959563b
Ant05 <delchi1199@gmail.com>
2018-02-25 18:39:59 +0100

    Update Radar.ino

--- robotica/Radar/Radar.ino
+++ robotica/Radar/Radar.ino
@@ -67,9 +67,11 @@ void loop() {
 
 }
 void Mappa(){
-   Serial.print(Distance0);
-   Serial.print("||");
+   Serial.print("Sensore1:");
+   Serial.println(Distance0);
+   Serial.print("Sensore2:");
    Serial.println(Distance1);
+   Serial.println("------");
    delay(700);
 }  
 void sensori(){
