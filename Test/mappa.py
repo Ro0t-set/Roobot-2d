24d3fab1cbcd6b02901a755b5604becca9539b6b
Ro0t-set <ro0trexustom@gmail.com>
2018-02-28 18:01:43 +0100

    vgjhk

--- Test/mappa.py
+++ Test/mappa.py
@@ -9,8 +9,7 @@ def distance():
 		a=str(a)
 		distance="read"+a
 		distance="Serial."+distance
-		distance=float(distance)
-
+		distance=eval(distance)
 		a=int(a)
 		angle=10*a
 		a=a+1
@@ -18,12 +17,11 @@ def distance():
 		
 
 def getXCorFromDistance(distance):
-	a=0
-	while a<36:
-		x = Int(sin(distance.angle)*distance.distance)
-		print (x)
-def getYCorFromDistance(distance):
-    y = Int(sin(12)*distance)
-    return y
-distance()
+	x = Int(cos(distance.angle)*distance.distance)
+	print (x)
+
+# def getYCorFromDistance(distance):
+#     y = Int(sin(12)*distance)
+#     return y
+
 getXCorFromDistance()
