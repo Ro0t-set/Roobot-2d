4f5c6db5350fd45af9277ca352e639929dcb0ce0
arcobiSwifty <arcobi98@gmail.com>
2018-02-28 18:27:51 +0100

    Test Add

--- Test/mappa.py
+++ Test/mappa.py
@@ -1,25 +1,25 @@
 import TestSeril
-import math 
+import math
 import os
 
 
 def distance():
 	a=0
+	angle = 0.0
 	while a<36:
 		a=str(a)
 		distance="read"+a
 		distance="TestSeril."+distance
 		distance=eval(distance)
 		a=int(a)
-		angle=10*a
 		x = int(math.cos(angle)*distance)
 		print (x)
-		a=a+1
+		angle += (math.pi*2)/ 36.0
+		a= a+1
+		
 
 distance()
 
 # def getYCorFromDistance(distance):
 #     y = Int(sin(12)*distance)
 #     return y
-
-
