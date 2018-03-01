1dd8ce09863dd16696e0612eb8d13c56ae1331d6
arcobiSwifty <arcobi98@gmail.com>
2018-03-01 13:51:14 +0100

    code refinements

--- Test/mappa.py
+++ Test/mappa.py
@@ -7,16 +7,17 @@ def distance():
 	a=0
 	angle = 0.0
 	while a<36:
-		a=str(a)
-		distance="read"+a
-		distance="TestSeril."+distance
-		distance=eval(distance)
-		a=int(a)
+		#a=str(a)
+		#distance="read"+a
+		#distance="TestSeril."+distance
+		#distance=eval(distance)
+		distance=eval("TestSeril.read"+str(a))
+		#a=int(a)
 		x = int(math.cos(angle)*distance)
 		y = int(math.sin(angle)*distance)
 		print (x)
 		print (y)
-		a=a+1
+		a+=1
 		angle += (math.pi*2)/35
 
 distance()
