0bf52afbb7d4b663d4ef78d39f8f4348943bb24d
Ro0t-set <ro0trexustom@gmail.com>
2018-03-05 17:51:27 +0100

    mega bug fix

--- robotica/rad.py
+++ robotica/rad.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # se si vuole usare quasto codice in test sensa i sensori sostituire tutti i "Serial" con "TestSerial"
-import Serial
+import TestSerial
 import math
 import os
 import sys
@@ -20,24 +20,31 @@ from app.models import Mappa
 nome_mappa =input(str("nome mappa:"))#selezionare l'id della mappa su cui si creerà il grafico
 def distance():
 	#loop di lettura dei dati distanze dal seriale
+	Rad90=((math.pi)*90)/180
 	a=0
-	angle = 0.0
+	angle = 0
 	while a<36:
 		a=str(a)
 		distance="read"+a
-		distance="Serial."+distance
+		distance="TestSerial."+distance
 		distance=eval(distance)#lettura distanza
 		distance=int(distance)
 		a=int(a)
-		angle += (math.pi*2)/ 36.0#calcolo angoli motore in radianti
-		x = int((math.cos(angle)*distance)/20)#creazione x e y per mezzo di seno e coseno, da lettura a cerchio a piano cartesiano
-		y = int((math.sin(angle)*distance)/20)
+		angle=angle+10
+		angleRad= a*(math.pi)/180#calcolo angoli motore in radianti
+		if a%2 == 0:
+			x = int((math.cos(angleRad)*distance)/10)#creazione x e y per mezzo di seno e coseno, da lettura a cerchio a piano cartesiano
+			y = int((math.sin(angleRad)*distance)/10)
+		else:
+			x = int((math.cos(angleRad+Rad90)*distance)/10)#creazione x e y per mezzo di seno e coseno, da lettura a cerchio a piano cartesiano
+			y = int((math.sin(angleRad+Rad90)*distance)/10)
+
 		print (x)
 		print (y)
 		a= a+1
 		try:
 			quadrato=Mappa.objects.get(x=x, y=y, nome_mappa=nome_mappa)#filtraggio dei dati per x, y e id mappa
-			quadrato.aggettivo=6#attribuzione di un aggettivo
+			quadrato.aggettivo=11#attribuzione di un aggettivo
 			quadrato.save()#salvataggio dati in Mappa
 
 
@@ -46,7 +53,7 @@ def distance():
 			pass
 
 
-grafo=Mappa.objects.filter(nome_mappa=nome_mappa, aggettivo=6)#filtraggio dati per aggettivo(comodo per lo sviluppo)
+grafo=Mappa.objects.filter(nome_mappa=nome_mappa, aggettivo=11)#filtraggio dati per aggettivo(comodo per lo sviluppo)
 print(grafo)
 distance()
 
