14facb9623fd3223e4877df6ce452e15ce93d51a
Ant05 <delchi1199@gmail.com>
2018-04-01 19:37:41 +0200

    Update Radar.ino

--- robotica/Radar/Radar.ino
+++ robotica/Radar/Radar.ino
@@ -7,62 +7,59 @@
 #define ECHO_PIN1  8
 #define MAX_DISTANCE  400
 
+
+int laser = 10;
 int Mseconds0 = 0;
 int Mseconds1 = 0;
 int Distance0 = 0;
 int Distance1 = 0; 
 int angolo;
 
+
+
+
 NewPing sonar0(TRIGGER_PIN0, ECHO_PIN0, MAX_DISTANCE); //Oggetto sonar 1
 NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); //Oggetto sonar 2
 
 Servo servomotore;
 
-char buffer[80] = { 0 };
 void setup(){
 Serial.begin (115200); //Comunicazione seriale 115200 bit
 servomotore.attach(3);
 pinMode(2,OUTPUT);
 pinMode(12,OUTPUT);
-pinMode(13,OUTPUT);
+pinMode(laser,OUTPUT);
 digitalWrite(2,HIGH);
 digitalWrite(12,HIGH);
-servomotore.write(180);
-Serial.println("test");
+servomotore.write(0);
+Serial.println("Ready");
 }
-/*Il loop comprende due funzioni; sensori e Mappa, attivate ogni 15 gradi di movimento del servomotore, 
+/*Il loop comprende due funzioni; sensori e Mappa, attivate ogni 5 gradi di movimento del servomotore, 
 sensori rileva le distanze, Mappa invia i valori al seriale, ogni ciclo del radar produce 24 valori in centimetri*/
 void loop() {
+  Blink();
+ 
+if (Serial.available() > 0){
 
-if (Serial.available() > 0) {
-  
-  
-  Serial.readBytesUntil('\n', buffer, sizeof(buffer));
-  Serial.println(buffer);
-  int incremento = atoi(buffer);
-  memset(buffer, 0, sizeof(buffer));
- // Serial.print("valore inserito = ");
- // Serial.println(incremento);
-angolo = 0;
+
+angolo=0;
+ 
 while(angolo<180)
-{
-   angolo = angolo+incremento;
-   digitalWrite(13,HIGH);
+{  
+   digitalWrite(laser,HIGH);
+   angolo = angolo+5;
    servomotore.write(angolo);
    sensori();
    Mappa();
-   delay(700);
-   digitalWrite(13,LOW);
-}if (angolo == 180){
-Serial.println("end");
+   delay(500);
 }
+Serial.end();
+servomotore.write(0);
 }
 }
 void Mappa(){
   Serial.println(Distance0);
   Serial.println(Distance1);
-  //Serial.print("angolo = ");//commenta questa riga dopo aver eseguito il test
-  //Serial.println(angolo);//commenta questa riga dopo aver eseguito il test
 }  
 void sensori(){
    Mseconds0 = sonar0.ping_median(5);
@@ -70,3 +67,10 @@ void sensori(){
    Distance0 = sonar0.convert_cm(Mseconds0);
    Distance1 = sonar1.convert_cm(Mseconds1);
 }
+void Blink(){
+  digitalWrite(laser, HIGH);
+  delay(500);
+  digitalWrite(laser, LOW);
+  delay(500);
+  
+  }
