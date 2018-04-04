84cd49c8b65a0a3decab68be5c91f645128d207a
Ro0t-set <ro0trexustom@gmail.com>
2018-04-04 21:07:34 +0200

    2 to 4 spages

--- robotica/movimento.py
+++ robotica/movimento.py
@@ -8,51 +8,44 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT) #pin1 motore1
 GPIO.setup(18, GPIO.OUT) #pin2 motore1
 GPIO.setup(24, GPIO.OUT) #pin1 motore2
-GPIO.setup(25, GPIO.OUT) 
+GPIO.setup(25, GPIO.OUT)
 
-def avanti(distanzaTemporale)
-  GPIO.output(18, GPIO.HIGH)
-  GPIO.output(25, GPIO.HIGH)
-  time.sleep(distanzaTemporale)
-  GPIO.output(18, GPIO.LOW)
-  GPIO.output(25, GPIO.LOW)
-  GPIO.output(23, GPIO.LOW)
-  GPIO.output(24, GPIO.LOW)
-return 0
+def avanti(distanzaTemporale):
+    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(25, GPIO.HIGH)
+    time.sleep(distanzaTemporale)
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(25, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    return 0
 
-def indietro(distanzaTemporale)
-  GPIO.output(18, GPIO.LOW)
-  GPIO.output(25, GPIO.LOW)
-  GPIO.output(23, GPIO.HIGH)
-  GPIO.output(24, GPIO.HIGH)
-  time.sleep(distanzaTemporale)
-  GPIO.output(23, GPIO.LOW)
-  GPIO.output(24, GPIO.LOW)
-return 0
+def indietro(distanzaTemporale):
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(25, GPIO.LOW)
+    GPIO.output(23, GPIO.HIGH)
+    GPIO.output(24, GPIO.HIGH)
+    time.sleep(distanzaTemporale)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    return 0
 
-def destra(distanzaTemporale)
-  GPIO.output(18, GPIO.LOW)
-  GPIO.output(25, GPIO.HIGH)
-  GPIO.output(23, GPIO.HIGH)
-  GPIO.output(24, GPIO.LOW)
-  time.sleep(distanzaTemporale)
-  GPIO.output(25, GPIO.LOW)
-  GPIO.output(23, GPIO.LOW)
-return 0
+def destra(distanzaTemporale):
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(25, GPIO.HIGH)
+    GPIO.output(23, GPIO.HIGH)
+    GPIO.output(24, GPIO.LOW)
+    time.sleep(distanzaTemporale)
+    GPIO.output(25, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+    return 0
 
-def sinistra(distanzaTemporale)
-  GPIO.output(18, GPIO.HIGH)
-  GPIO.output(24, GPIO.HIGH)
-  time.sleep(distanzaTemporale)
-  GPIO.output(18, GPIO.LOW)
-  GPIO.output(24, GPIO.LOW)  
-  GPIO.output(25, GPIO.LOW)
-  GPIO.output(23, GPIO.LOW) 
-return 0
-
-def stop()
-  GPIO.output(18, GPIO.LOW)
-  GPIO.output(25, GPIO.LOW)
-  GPIO.output(23, GPIO.LOW)
-  GPIO.output(24, GPIO.LOW)
-return 0
+def sinistra(distanzaTemporale):
+    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(24, GPIO.HIGH)
+    time.sleep(distanzaTemporale)
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    GPIO.output(25, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+    return 0
