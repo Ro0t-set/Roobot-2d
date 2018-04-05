03812c7f87c2e273159f07df6518e9ce514ff547
Ant05 <delchi1199@gmail.com>
2018-04-06 00:32:45 +0200

    Update Radar.ino

--- Radar/Radar.ino
+++ Radar/Radar.ino
@@ -5,7 +5,7 @@
 #define ECHO_PIN0  7
 #define TRIGGER_PIN1  6
 #define ECHO_PIN1  8
-#define MAX_DISTANCE  400
+#define MAX_DISTANCE  100
 
 
 int laser = 10;
@@ -24,7 +24,7 @@ NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); //Oggetto sonar 2
 Servo servomotore;
 
 void setup(){
-Serial.begin (115200); //Comunicazione seriale 115200 bit
+Serial.begin (115200); 
 servomotore.attach(3);
 pinMode(2,OUTPUT);
 pinMode(12,OUTPUT);
@@ -52,20 +52,32 @@ while(angolo<180)
    sensori();
    Mappa();
    delay(500);
+  
 }
 Serial.end();
 servomotore.write(0);
+
 }
 }
+ 
+
 void Mappa(){
   Serial.println(Distance0);
   Serial.println(Distance1);
-}  
+  }  
 void sensori(){
    Mseconds0 = sonar0.ping_median(5);
    Mseconds1 = sonar1.ping_median(5);
    Distance0 = sonar0.convert_cm(Mseconds0);
    Distance1 = sonar1.convert_cm(Mseconds1);
+    while (Distance0 == 0)
+ {
+   Distance0 = 101;
+  }
+  while (Distance1 == 0)
+ {
+   Distance1 = 101;
+  }
 }
 void Blink(){
   digitalWrite(laser, HIGH);
